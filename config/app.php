c3ed0399ee64aaf619eb2a6ed95e3db4ec709a20
Shubham <shubhamkrverma73@gmail.com>
2021-02-26 20:59:55 +0530

    localhost path changes

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://localhost/laravel_broadcast/'),
 
     'asset_url' => env('ASSET_URL', null),
 
